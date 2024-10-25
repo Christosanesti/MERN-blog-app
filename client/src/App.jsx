df1c908e8a05cfba49526d1ef30a15ecf6a603dc
Paisios <navophoto@protonmail.com>
2024-10-25 15:25:19 +0330

    App Router and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,22 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import History from "./pages/History";
+import Dashboard from "./pages/Dashboard";
 export default function App() {
-  return <h1 className="text-3xl text-red-600">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>\
+        <Route path="/history" element={<History />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
